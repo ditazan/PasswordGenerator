97a74532eb3a1583ef076146d29a4d030377c499
dit <ditazanelli@gmail.com>
2021-12-07 19:00:54 -0600

    length of password

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,35 @@
-// Assignment code here
+// prompt for length of password
+function passLength() {
+  var pass = [];
+  var character = window.prompt(
+    "how long would you like your password? (password must be a minimum of 8 characters and a max of 128)."
+  );
+  var character = parseInt(character);
+  if (character < 8 || character > 128) {
+    window.alert(
+      "invalid length. (password must be a minimum of 8 characters and a max of 128)"
+    );
+  } else {
+    pass.length = character;
+  }
+  console.log(character);
+  console.log(`character # ${character}`);
+  console.log(`password length ${pass.length}`);
 
+  for (i = 0; i < character; i++) {
+    console.log(pass[i]);
+  }
+  return character;
+  // double checking values are correct and character length / array length are changing properly
+}
+console.log(`passLength func returns: ${passLength()}`);
+
+function generatePassword() {
+  //prompt for character types
+  // var lCase= window.prompt("Would you like LOWERCASE letters in your password? y/n")
+  // let LCAnswer = lCase.toLowerCase();
+  // if (LCAnswer === "y"){
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -10,7 +40,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
